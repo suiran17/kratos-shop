33207b85e8b563426c51d4814f815244c49ad92a
zhangpan <2223410846@qq.com>
2023-03-04 02:14:15 +0800

    四. BFF 层

---
+++ shop/internal/service/service.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	v1 "shop/api/shop/v1"
+	"shop/internal/biz"
+
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+)
+
+// ProviderSet is service providers.
+var ProviderSet = wire.NewSet(NewShopService)
+
+// ShopService is a shop service.
+type ShopService struct {
+	v1.UnimplementedShopServer
+
+	uc  *biz.UserUsecase
+	log *log.Helper
+}
+
+// NewShopService new a shop service.
+func NewShopService(uc *biz.UserUsecase, logger log.Logger) *ShopService {
+	return &ShopService{
+		uc:  uc,
+		log: log.NewHelper(log.With(logger, "module", "service/shop")),
+	}
+}
