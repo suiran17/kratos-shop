b838e8cd53132dbd2f92bbd0bc047ff1453a3130
zhangpan <2223410846@qq.com>
2023-03-03 15:10:11 +0800

    实现 rpc 并进行 data/user_test.go单元测试

--- service/user/internal/biz/user.go
+++ service/user/internal/biz/user.go
@@ -2,19 +2,25 @@ package biz
 
 import (
 	"context"
+	"time"
 
 	"github.com/go-kratos/kratos/v2/log"
+	"gorm.io/gorm"
 )
 
 // User 定义返回数据结构体
 type User struct {
-	ID       int64
-	Mobile   string
-	Password string
-	NickName string
-	Birthday int64
-	Gender   string
-	Role     int
+	ID          int64
+	Mobile      string
+	Password    string
+	NickName    string
+	Birthday    *time.Time
+	Gender      string
+	Role        int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	DeletedAt   gorm.DeletedAt
+	IsDeletedAt bool
 }
 
 // UserRepo 注意这一行新增的 mock 数据的命令
@@ -22,6 +28,11 @@ type User struct {
 //go:generate mockgen -destination=../mocks/mrepo/user.go -package=mrepo . UserRepo
 type UserRepo interface {
 	CreateUser(context.Context, *User) (*User, error)
+	ListUser(ctx context.Context, pageNum, pageSize int) ([]*User, int, error)
+	UserByMobile(ctx context.Context, mobile string) (*User, error)
+	GetUserById(ctx context.Context, id int64) (*User, error)
+	UpdateUser(context.Context, *User) (bool, error)
+	CheckPassword(ctx context.Context, password, encryptedPassword string) (bool, error)
 }
 
 type UserUsecase struct {
@@ -35,4 +46,24 @@ func NewUserUsecase(repo UserRepo, logger log.Logger) *UserUsecase {
 
 func (uc *UserUsecase) Create(ctx context.Context, u *User) (*User, error) {
 	return uc.repo.CreateUser(ctx, u)
+}
+
+func (uc *UserUsecase) List(ctx context.Context, pageNum, pageSize int) ([]*User, int, error) {
+	return uc.repo.ListUser(ctx, pageNum, pageSize)
+}
+
+func (uc *UserUsecase) UserByMobile(ctx context.Context, mobile string) (*User, error) {
+	return uc.repo.UserByMobile(ctx, mobile)
+}
+
+func (uc *UserUsecase) UpdateUser(ctx context.Context, user *User) (bool, error) {
+	return uc.repo.UpdateUser(ctx, user)
+}
+
+func (uc *UserUsecase) CheckPassword(ctx context.Context, password, encryptedPassword string) (bool, error) {
+	return uc.repo.CheckPassword(ctx, password, encryptedPassword)
+}
+
+func (uc *UserUsecase) UserById(ctx context.Context, id int64) (*User, error) {
+	return uc.repo.GetUserById(ctx, id)
 }
